43957db5f37735acb046167fabd171595e4a37fa
Santiago Salazar <santiago2sala@hotmail.com>
2025-08-23 11:22:50 +1000

    fixing api endpoint enviroment variable

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,9 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   output: "standalone",
+  env: {
+    API_ENDPOINT: process.env.API_ENDPOINT,
+  },
 };
 
 export default nextConfig;
